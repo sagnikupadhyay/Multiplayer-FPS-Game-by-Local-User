380cada809af49d23c1dab4b4852854ec7a440f4
sagnikupadhyay <sagnikupadhyay.2005@gmail.com>
2021-01-22 22:55:29 +0530

    First MultiPlayerFPS Tutorial
    Added my Unity project

---
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class Player : NetworkBehaviour
+{
+    [SyncVar]
+    private bool _isDead = false;
+    public bool isDead
+    {
+        get { return _isDead; }
+        protected set { _isDead = value; }
+    }
+    [SerializeField]
+    private int maxHealth = 100;
+
+    [SyncVar]
+    private int currentHealth;
+    [SerializeField]
+    private Behaviour[] disableOnDeath;
+    private bool[] wasEnabled;
+
+    void Setup ()
+    {
+        SetDefaults();
+    }
+
+    [ClientRpc]
+    public void RpcTakeDamage(int _amount)
+    {
+        if (isDead)
+            return;
+        currentHealth -= _amount;
+        Debug.Log(transform.name + " now has " + currentHealth + " health.");
+
+        if(currentHealth<=0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        isDead = true;
+
+        //DISABLE COMPONENTS
+        Debug.Log(transform.name + " is Dead!");
+
+        //CALL RESPAWN METHOD
+    }
+
+    public void SetDefaults()
+    {
+        currentHealth = maxHealth;
+    }
+}
---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 10f;
+    [SerializeField]
+    private float sensitivity = 4f;    
+
+    private PlayerMotor motor;   
+
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+       
+    }
+    
+    void Update()
+    {
+        float _xmov = Input.GetAxisRaw("Horizontal");
+        float _zmov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _movHorizontal = transform.right * _xmov;
+        Vector3 _movVertical = transform.forward * _zmov;
+        
+        //final movement vector
+        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+
+        //apply movement
+        motor.Move(_velocity);
+
+        //Calculate rotation as a 3d vector.(use for turning around)
+        float _yrot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, _yrot, 0f) * sensitivity;
+
+        //apply rotation
+        motor.Rotate(_rotation);
+
+        //Calculate camera rotation as a 3d vector.(use for turning up and down)
+        float _xrot = Input.GetAxisRaw("Mouse Y");
+        float _cameraRotationX = _xrot * sensitivity;
+             
+        //apply camera rotation
+        motor.RotateCamera(_cameraRotationX);
+       
+    }
+    
+}
